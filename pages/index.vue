487761835534b5d223a0eb995dfc29c1939ec909
Hüseyin Can Çirkin <huseyin.can.cirkin@gmail.com>
2024-09-23 22:44:47 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
 import { onMounted } from "vue";
 
 async function login() {
-  await $fetch("http://localhost:8080/signup", {
+  await $fetch("http://34.243.145.118:8080/signup", {
     method: "POST",
     body: {
       email: "test3@test.com",
